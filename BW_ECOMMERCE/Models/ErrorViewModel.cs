c6008edf3f1ad13c5412ad03b5f2a982cb4048e3
GiampyLux <giampaolopaladino01@gmail.com>
2024-07-08 15:21:55 +0200

    Aggiungere i file di progetto.

---
+++ BW_ECOMMERCE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BW_ECOMMERCE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
