2bf5ccf1859ed259e363194546ad41758fdc4345
Ciraldos <andreaciraldo2@gmail.com>
2024-07-10 12:17:57 +0200

    .

--- BW_ECOMMERCE/Controllers/HomeController.cs
+++ BW_ECOMMERCE/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 using BW_ECOMMERCE.Models;
 using BW_ECOMMERCE.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Diagnostics;
 
 namespace BW_ECOMMERCE.Controllers
@@ -63,6 +61,13 @@ namespace BW_ECOMMERCE.Controllers
                 ModelState.AddModelError("", "Si è verificato un errore durante la creazione del prodotto.");
                 return View(prodotto);
             }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _prodottoService.DeleteProdotto(id);
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
