4ae9c879948ffd43155fec6b32fba9fe9e7d5624
Ciraldos <andreaciraldo2@gmail.com>
2024-07-11 14:46:21 +0200

    .

--- BW_ECOMMERCE/wwwroot/js/site.js
+++ BW_ECOMMERCE/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener('scroll', function () {
+    var gusta = document.getElementById('gusta');
+
+    // Verifica la posizione dello scroll
+    if (window.scrollY > 650) {
+        // Aggiungi la classe "scrolled" quando la pagina è scrollata più di 50px
+        gusta.classList.remove('myGreen');
+        gusta.classList.add('myOrange');
+    } else {
+        // Rimuovi la classe "scrolled" quando lo scroll è meno di 50px
+        gusta.classList.remove('myOrange');
+        gusta.classList.add('myGreen');
+
+    }
+});
