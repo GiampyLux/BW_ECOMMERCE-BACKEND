ae4c6cbb93b27eba0fba3f847a1fd9bb6a5f4b38
Ciraldos <andreaciraldo2@gmail.com>
2024-07-12 09:22:51 +0200

    .

---
+++ BW_ECOMMERCE/wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿window.addEventListener('scroll', function () {
+    var fruttaSection = document.getElementById('frutta');
+
+    // Verifica la posizione dello scroll
+    if (window.scrollY + window.innerHeight > fruttaSection.offsetTop) {
+        // Aggiungi la classe "scrolled" quando la sezione "frutta" è visibile
+        fruttaSection.classList.add('scrolled');
+    } else {
+        // Rimuovi la classe "scrolled" quando la sezione "frutta" non è più visibile
+        fruttaSection.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    var verduraSection = document.getElementById('verdura');
+
+    // Verifica la posizione dello scroll
+    if (window.scrollY + window.innerHeight > verduraSection.offsetTop) {
+        // Aggiungi la classe "scrolled" quando la sezione "frutta" è visibile
+        verduraSection.classList.add('scrolled');
+    } else {
+        // Rimuovi la classe "scrolled" quando la sezione "frutta" non è più visibile
+        verduraSection.classList.remove('scrolled');
+    }
+});
